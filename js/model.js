5d44c1f44ba971fd97936772e78e6ce1b31d65a8
Cristian Cortés <cristian.cortes@brm.com.co>
2016-03-30 19:22:03 -0500

    modificado json y html

--- js/model.js
+++ js/model.js
@@ -1,53 +1,27 @@
-/*var Biblioteca = Backbone.Model.extend({
-	titulo: '',
-	titulo: '',
-	titulo: '',
-	toTemplateJSON: function() {
-		var json = this.toJSON();
-		json.area = this.area();
-		return json;
-	}
-});
+var pokemon = Backbone.Model.extend({});
 
-titulo: '',*/
+ // url: 'https://www.googleapis.com/books/v1/volumes?q=python&maxResults=10&startIndex=20&fields=totalItems,items%28id,volumeInfo/title,volumeInfo/subtitle,volumeInfo/authors,volumeInfo/publishedDate,volumeInfo/description,volumeInfo/imageLinks%29',
 
-// var libro = Backbone.Model.extend({});
-/*var Libro = Backbone.Model.extend({
-    id:[]
-});
-*/
-var libro = Backbone.Model;
-
-
-
-var NoteStore = Backbone.Collection.extend({
-    model: libro,
-    url: 'https://www.googleapis.com/books/v1/volumes?q=python&maxResults=10&startIndex=20&fields=totalItems,items%28id,volumeInfo/title,volumeInfo/subtitle,volumeInfo/authors,volumeInfo/publishedDate,volumeInfo/description,volumeInfo/imageLinks%29',
+var Pokedex = Backbone.Collection.extend({
+    model: pokemon,
+    url: 'pokemons.json',
     parse: function( response ) {
         return response;
     },
-    // Overwrite the sync method to pass over the Same Origin Policy
-        sync: function(method, model, options) {
-            var that = this;
-                var params = _.extend({
-                    type: 'GET',
-                    dataType: 'jsonp',
-                    url: that.url,
-                    processData: false
-                }, options);
-
-            return $.ajax(params);
-        }
 });
 
 
-var viewLista = Backbone.View.extend({
-	 
-	 initialize: function () {
-	 	
-	 	_.bindAll(this, 'render');
+
+
+var viewPokedex = Backbone.View.extend({
+
+  el: $('.row-render'),
+  template: _.template($('#pokemon-template').html()),
+   
+   initialize: function () {
+  
       // create a collection
-      this.collection = new NoteStore();
+      this.collection = new Pokedex();
       // Fetch the collection and call render() method
       var that = this;
       this.collection.fetch({
@@ -55,38 +29,17 @@ var viewLista = Backbone.View.extend({
             that.render();
         }
       });
-	 },
-	 
-	 template: _.template($('#libros-template').html()),
-	 
-	 render: function() {
+   },
+   
+   
+   render: function() {
         // Fill the html with the template and the collection
-        $(this.el).html(this.template({ libro: this.collection.toJSON() }));
-    },
-	 
+        this.$el.html(this.template({ pokemon: this.collection.toJSON() }));
+        // this.$el.html('hola');
+        return this
+    }
+   
 
 });
 
-var app = new viewLista({
-    // define the el where the view will render
-    el: $('.row-render')
-});
-
-/*
-var viewLista = new viewLista();
-
- var Router = Backbone.Router.extend({
-      routes: {
-        '': 'home'
-      }
-    });
-
-    var router = new Router();
-    router.on('route:home', function () {
-      viewLista.render();
-    });
-
-
-var notes = new NoteStore();
-console.log(notes);
-notes.fetch();*/
+var app = new viewPokedex;
